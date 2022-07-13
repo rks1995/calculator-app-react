d06a8e9f0827c6fd2646326b3a2cad0b24b7c8ea
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-07-13 19:14:41 +0530

    calculator ui create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calculator-app'>
+      <h2 className='heading'>Calculate</h2>
+      <div className='input-field'>
+        <input type='text' />
+      </div>
+      <div className='grid'>
+        <button className='btn red'>C</button>
+        <button className='btn blue'>(</button>
+        <button className='btn blue'>)</button>
+        <button className='btn yellow'>/</button>
+        <button className='btn blue'>7</button>
+        <button className='btn blue'>8</button>
+        <button className='btn blue'>9</button>
+        <button className='btn yellow'>*</button>
+        <button className='btn blue'>4</button>
+        <button className='btn blue'>5</button>
+        <button className='btn blue'>6</button>
+        <button className='btn yellow'>+</button>
+        <button className='btn blue'>1</button>
+        <button className='btn blue'>2</button>
+        <button className='btn blue'>3</button>
+        <button className='btn yellow'>-</button>
+        <button className='btn blue'>0</button>
+        <button className='btn blue'>00</button>
+        <button className='btn blue'>.</button>
+        <button className='btn green'>=</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
